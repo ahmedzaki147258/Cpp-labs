5ec270024dcc3132a78f790ec173cdb59f2f4c86
Ahmed zaki <Azaki@gmail.com>
2024-12-09 05:48:44 +0200

    day6

--- day4/include/Picture.h
+++ day4/include/Picture.h
@@ -5,8 +5,8 @@ class Point {
 private:
     int x, y;
 public:
-    Point() : x(0), y(0) {}
-    Point(int x1, int y1) : x(x1), y(y1) {}
+    Point(){ x=0; y=0; }
+    Point(int x, int y){ this->x = x; this->y = y; }
 
     int getX() { return x; }
     void setX(int x){ this->x = x; }
@@ -18,24 +18,17 @@ class Shape{
 protected:
     int color;
 public:
-    Shape(int color){
-        this->color = color;
-    }
-
-    int getColor(){
-        return color;
-    }
-
-    void setColor(int newColor){
-        color = newColor;
-    }
+    Shape(int color){ this->color = color; }
+    int getColor(){ return color; }
+    void setColor(int color){ this->color = color; }
+    virtual void draw()=0;
 };
 
 class Line : public Shape {
+protected:
+    Point start; // Composition
+    Point end;   // Composition
 public:
-    Point start;
-    Point end;
-// public:
     Line() : start(), end(), Shape(WHITE) {}
     Line(int x1, int y1, int x2, int y2, int color) : start(x1, y1), end(x2, y2), Shape(color) {}
 
@@ -44,20 +37,13 @@ public:
         line(start.getX(), start.getY(), end.getX(), end.getY());
         setcolor(WHITE);
     }
-    void move(int dx, int dy) {
-        start.setX(start.getX() + dx);
-        start.setY(start.getY() + dy);
-        end.setX(end.getX() + dx);
-        end.setY(end.getY() + dy);
-    }
-
 };
 
 class Rect : public Shape {
+protected:
+    Point ul; // Upper-left point  // Composition
+    Point lr; // Lower-right point // Composition
 public:
-    Point ul; // Upper-left point
-    Point lr; // Lower-right point
-// public:
     Rect() : ul(), lr(), Shape(WHITE) {}
     Rect(int x1, int y1, int x2, int y2, int color) : ul(x1, y1), lr(x2, y2), Shape(color) {}
 
@@ -66,90 +52,56 @@ public:
         rectangle(ul.getX(), ul.getY(), lr.getX(), lr.getY());
         setcolor(WHITE);
     }
-    void move(int dx, int dy) {
-        ul.setX(ul.getX() + dx);
-        ul.setY(ul.getY() + dy);
-        lr.setX(lr.getX() + dx);
-        lr.setY(lr.getY() + dy);
-    }
 };
 
 class Circle : public Shape {
 public:
-    Point center;
+    Point center;  // Composition
     int radius;
 
-    Circle() : center(), radius(0), Shape(WHITE) {}
-    Circle(int m, int n, int r, int color) : center(m, n), radius(r), Shape(color) {}
+    Circle() : center(), Shape(WHITE) { radius = 0; }
+    Circle(int m, int n, int r, int color) : center(m, n), Shape(color) { radius = r; }
 
     void draw() {
         setcolor(color);
         circle(center.getX(), center.getY(), radius);
         setcolor(WHITE);
     }
-    void move(int dx, int dy) {
-        center.setX(center.getX() + dx);
-        center.setY(center.getY() + dy);
-    }
 };
 
 class Picture{
 private:
-    int cNum, rNum, lNum;
-    Circle* pCircles;
-    Rect* pRects;
-    Line* pLines;
+    Shape** shapes; // Aggregation
+    int size, capacity;
 
 public:
-    Picture(){
-        cNum=0;
-        rNum=0;
-        lNum=0;
-        pCircles = nullptr;
-        pRects = nullptr;
-        pLines = nullptr;
+    Picture(int capacity){
+        size = 0;
+        shapes = new Shape*[capacity];
     }
 
-    void setCircles(int cn, Circle* pC) {
-        cNum = cn;
-        pCircles = pC;
+    void addShape(Shape* shape) {
+        shapes[size] = shape;
+        size++;
     }
-
-    void setRects(int rn, Rect* pR) {
-        rNum = rn;
-        pRects = pR;
-    }
-
-    void setLines(int ln, Line* pL) {
-        lNum = ln;
-        pLines = pL;
-    }
-
     void paint() {
-        for (int i = 0; i < cNum; i++) {
-            pCircles[i].draw();
-        }
-        for (int i = 0; i < rNum; i++) {
-            pRects[i].draw();
-        }
-        for (int i = 0; i < lNum; i++) {
-            pLines[i].draw();
+        for (int i = 0; i < size; i++) {
+            shapes[i]->draw();
         }
     }
 
     void updateColors() {
-        for (int i = 0; i < cNum; i++) {
-            int newColor = (pCircles[i].getColor() + 1) % 15;
-            pCircles[i].setColor(newColor);
+        for (int i = 0; i < size; i++) {
+            int newColor = (shapes[i]->getColor() + 1) % 15;
+            shapes[i]->setColor(newColor);
         }
-        for (int i = 0; i < rNum; i++) {
-            int newColor = (pRects[i].getColor() + 1) % 15;
-            pRects[i].setColor(newColor);
+    }
+
+    ~Picture() {
+        for (int i = 0; i < size; i++) {
+            delete shapes[i];
         }
-        for (int i = 0; i < lNum; i++) {
-            int newColor = (pLines[i].getColor() + 1) % 15;
-            pLines[i].setColor(newColor);
-        }
+        delete[] shapes;
     }
 };
 
--- day4/main.cpp
+++ day4/main.cpp
@@ -4,88 +4,37 @@
 using namespace std;
 
 int main(){
-    Picture myPic;  
+    Picture myPic(15);  
     int gd = DETECT, gm; 
-    initgraph(&gd, &gm, ""); 
+    initgraph(&gd, &gm, "");
 
-    srand(time(0)); // Seed for random number generation
+    myPic.addShape(new Circle(50, 50, 50, 1));
+    myPic.addShape(new Circle(200, 100, 100, 2));
+    myPic.addShape(new Circle(420, 50, 30, 3));
+    myPic.addShape(new Circle(300, 300, 70, 4));
+    myPic.addShape(new Circle(150, 400, 50, 5));
 
-    Circle cArr[3] = { Circle(50, 50, 50, 1), Circle(200, 100, 100, 2), Circle(420, 50, 30, 3) };
-    Rect rArr[2] = { Rect(30, 40, 170, 100, 4), Rect(420, 50, 500, 300, 5) };
-    Line lArr[2] = { Line(420, 50, 300, 300, 12), Line(40, 500, 500, 400, 14) };
+    myPic.addShape(new Rect(30, 40, 170, 100, 6));
+    myPic.addShape(new Rect(420, 50, 500, 300, 7));
+    myPic.addShape(new Rect(200, 200, 300, 300, 8));
+    myPic.addShape(new Rect(100, 100, 250, 200, 9));
 
-    myPic.setCircles(sizeof(cArr)/sizeof(cArr[0]), cArr);
-    myPic.setRects(sizeof(rArr)/sizeof(rArr[0]), rArr);
-    myPic.setLines(sizeof(lArr)/sizeof(lArr[0]), lArr);
+    myPic.addShape(new Line(420, 50, 300, 300, 10));
+    myPic.addShape(new Line(40, 500, 500, 400, 11));
+    myPic.addShape(new Line(150, 150, 450, 150, 12));
+    myPic.addShape(new Line(250, 350, 350, 450, 13));
+    myPic.addShape(new Line(50, 50, 500, 500, 14));
 
-    float timeStep = 0;
     while (!kbhit()) {
-        for (int i = 0; i < 3; i++) {
-            float dx = 5;
-            float dy = 20 * sin(timeStep + i);
-            cArr[i].move(dx, dy);
-        }
-
-        for (int i = 0; i < 2; i++) {
-            float dx = 5;
-            float dy = 15 * sin(timeStep + i);
-            rArr[i].move(dx, dy);
-        }
-
-        for (int i = 0; i < 2; i++) {
-            float dx = 5;
-            float dy = 10 * sin(timeStep + i);
-            lArr[i].move(dx, dy);
-        }
-
-        timeStep += 0.1;
         cleardevice();
         myPic.paint();
         delay(700);
         myPic.updateColors();
     }
-
+    
     closegraph();
     return 0;
 }
 
 // g++ main.cpp -lSDL_bgi -lSDL2
 // ./a.out
-
-// BLACK                   0
-// BLUE                    1
-// GREEN                   2
-// CYAN                    3   
-// RED                     4
-// MAGENTA                 5
-// BROWN                   6 
-// LIGHTGRAY               7 
-// DARKGRAY                8
-// LIGHTBLUE               9
-// LIGHTGREEN             10
-// LIGHTCYAN              11
-// LIGHTRED               12
-// LIGHTMAGENTA           13
-// YELLOW                 14
-// WHITE                  15
-
-    // Circle cArr[] = { 
-    //     Circle(50, 50, 50, 1), 
-    //     Circle(200, 100, 100, 2), 
-    //     Circle(420, 50, 30, 3), 
-    //     Circle(300, 300, 70, 4), 
-    //     Circle(150, 400, 50, 5) 
-    // };
-    // Rect rArr[] = { 
-    //     Rect(30, 40, 170, 100, 6), 
-    //     Rect(420, 50, 500, 300, 7), 
-    //     Rect(200, 200, 300, 300, 8), 
-    //     Rect(100, 100, 250, 200, 9) 
-    // };
-    // Line lArr[] = { 
-    //     Line(420, 50, 300, 300, 10), 
-    //     Line(40, 500, 500, 400, 11), 
-    //     Line(150, 150, 450, 150, 12), 
-    //     Line(250, 350, 350, 450, 13), 
-    //     Line(50, 50, 500, 500, 14) 
-    // };
